c0d13bfde85c45351859c3eb9119c155cd4a99cb
pengpengyang <lyana.q669@gmail.com>
2024-02-04 01:03:04 +0200

    modify header& footer PHP

---
+++ CoffeeShop/HomePage.php
@@ -0,0 +1,15 @@
+<?php
+$title = "Home Page";
+$md="Please use appropriate meta Description";
+include 'header.php'; ?>
+
+
+<div class="word">
+            <p class="big">Good Coffee, Good Taste, For Good People</p>
+            <p class="small">Here in this cold winter season, you can get a great cup of coffee in your hand and talk
+                with your friends.</p>
+            <button class="button2"><a href="#" class="link">Read More</a></button>
+        </div>
+        <div class="coffee"></div>
+
+<?php include 'footer.php'; ?>
---
+++ CoffeeShop/footer.php
@@ -0,0 +1,44 @@
+</section>
+
+    <footer class="container-fluid d-flex align-items-start justify-content-center">
+        <div class="row" style="width: 1240px; margin-top: 20px">
+            <div class="col-md">
+                <p style="font-weight: bolder; font-size: 25px">Information</p>
+                <p>Behind the word mountains, far from the countries Vokalia and Consonantia</p>
+
+                <div class="row d-flex">
+                    <div class="linkMethod2">
+                        <i class="fab fa-facebook"
+                            style="top: 50%;left: 50%; position: absolute; transform: translate(-50%, -50%);"></i>
+                    </div>
+
+                    <div class="linkMethod2">
+                        <i class="fab fa-twitter-square"
+                            style="top: 50%;left: 50%; position: absolute; transform: translate(-50%, -50%);"></i>
+                    </div>
+
+                    <div class="linkMethod2">
+                        <i class="fab fa-instagram-square"
+                            style="top: 50%;left: 50%; position: absolute; transform: translate(-50%, -50%);"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md">
+                <p style="font-weight: bolder; font-size: 25px">Useful Links</p>
+                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="#">Contact</a>
+                <a class="nav-link" href="#">Shop Now</a>
+            </div>
+
+            <div class="col-md">
+                <p style="font-weight: bolder; font-size: 25px">Opening Hours</p>
+                <p>Mon-Fri: 8a.m to 9p.m</p>
+                <p>Sat-Sun: 10a.m to 10p.m</p>
+            </div>
+        </div>
+        <div></div>
+    </footer>
+</body>
+
+</html>
