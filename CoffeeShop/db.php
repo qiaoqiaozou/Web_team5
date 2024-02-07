f2cb49086fb1c768a8880c672d3f7c65795306e0
pengpengyang <lyana.q669@gmail.com>
2024-02-07 14:04:50 +0200

    modify

--- CoffeeShop/db.php
+++ CoffeeShop/db.php
@@ -8,7 +8,8 @@ $password = "password";
 $dbname = "crudAPP";
 
 
-/* 最后上传shell所需个人数据库
+/*
+//最后上传shell所需个人数据库
 $servername = "localhost";
 $username = "pengpeng23000";
 $password = "rTaPFrQ6";
@@ -19,7 +20,7 @@ $dbname = "wp_pengpeng23000";
 $conn = new mysqli($servername,$username,$password,$dbname);
 
 if ($conn->connect_error){
-    die("Vonnection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 //echo "Connected successfully";
 
