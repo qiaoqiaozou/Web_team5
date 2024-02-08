447c020e13556e9b6edd8f4318627163ff4e318f
Ni.Xiong <xiong3ni@hotmail.com>
2024-02-08 17:22:26 +0200

    header footer files

--- CoffeeShop/footerY.php
+++ CoffeeShop/footerY.php
@@ -1,36 +1,33 @@
-<footer class="container-fluid d-flex align-items-start justify-content-center">
-        <div class="row" style="width: 1240px;margin-top: 20px">
+</section>
+
+    <footer class="container-fluid d-flex align-items-start justify-content-center">
+        <div class="row" style="width: 1240px; margin-top: 20px">
             <div class="col-md">
                 <p style="font-weight: bolder; font-size: 25px">Information</p>
-                <p>Behind the word mountains, far from the countries Vokalia and Consonantia, there</p>
-
-
+                <p>Behind the word mountains, far from the countries Vokalia and Consonantia</p>
 
                 <div class="row d-flex">
                     <div class="linkMethod2">
-                        <i class="fa-brands fa-facebook"
+                        <i class="fab fa-facebook"
                             style="top: 50%;left: 50%; position: absolute; transform: translate(-50%, -50%);"></i>
                     </div>
 
                     <div class="linkMethod2">
-                        <i class="fa-brands fa-square-x-twitter"
+                        <i class="fab fa-twitter-square"
                             style="top: 50%;left: 50%; position: absolute; transform: translate(-50%, -50%);"></i>
                     </div>
-
 
                     <div class="linkMethod2">
-                        <i class="fa-brands fa-square-instagram"
+                        <i class="fab fa-instagram-square"
                             style="top: 50%;left: 50%; position: absolute; transform: translate(-50%, -50%);"></i>
                     </div>
-
                 </div>
-
             </div>
 
             <div class="col-md">
                 <p style="font-weight: bolder; font-size: 25px">Useful Links</p>
-                <a class="nav-link" href="index.html">Home</a>
-                <a class="nav-link" href="contactUs_JiaweiLi.html">Contact</a>
+                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="#">Contact</a>
                 <a class="nav-link" href="#">Shop Now</a>
             </div>
 
@@ -42,7 +39,6 @@
         </div>
         <div></div>
     </footer>
-    </div>
 </body>
 
 </html>
